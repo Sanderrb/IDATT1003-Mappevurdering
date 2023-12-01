6aadfe6b305b9a3a1de88228b72c9f5867d796a2
Sander Rusten Berge <sanderrb@stud.ntnu.no>
2023-12-01 12:17:56 +0100

    In this commit I have continued on the TrainDispatchSystem. I have added the necessary booleans for the system. These include methods such as: addTrainDeparture, setTrackForDeparture, addDelayToDeparture, setCurrentTime.

--- src/main/java/edu/ntnu/stud/TrainDispatchSystem.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchSystem.java
@@ -3,6 +3,7 @@ package edu.ntnu.stud;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,7 +38,47 @@ public List<TrainDeparture> searchByDestination(String destination) {
             .collect(Collectors.toList());
 }
 
+//Method to add a new traindeparture
+public boolean addTrainDeparture(TrainDeparture departure) {
+    if (departures.stream().anyMatch(d -> d.getTrainNumber().equals(departure.getTrainNumber()))) {
+        return false; //It returns false if the trainnumber already exists. Therefore cant be placed in the system
+    }
+    departures.add(departure);
+    departures.sort(Comparator.comparing(TrainDeparture::getDepartureTime));
+    return true;
+}
 
+//This method sets the track for a traindeparture
+public boolean setTrackForDeparture(String trainNumber, int track) {
+    for (TrainDeparture departure : departures) {
+        if (departure.getTrainNumber().equals(trainNumber)) {
+            departure.setTrack(track);
+            return true;
+        }
+    }
+    return false;
+}
+
+//This method adds a delay for a traindeparture if needed
+public boolean addDelayToDeparture(String trainNumber, String delay) {
+    for (TrainDeparture departure : departures) {
+        if (departure.getTrainNumber().equals(trainNumber)) {
+            departure.setDelay(delay);
+            return true;
+        }
+    }
+    return false;
+}
+
+//This method updates the current time
+public boolean setCurrentTime(String newTimeStr) {
+    LocalTime newTime = LocalTime.parse(newTimeStr, DateTimeFormatter.ofPattern("HH:mm"));
+    if (newTime.isAfter(currentTime) || newTime.equals(currentTime)) {
+        currentTime = newTime;
+        return true;
+    }
+    return false;
+}
 
 
 
