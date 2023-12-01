30284e6279c570a8af1a4bb65d4d76fd06dc62e0
Sander Rusten Berge <sanderrb@stud.ntnu.no>
2023-12-01 13:58:46 +0100

    I continued working on TrainDispatchSystem class. I added several methods including: displayDepartures, displayDeparturesAfterCurrentTime, removeDeparturesBefore. Lastly I added some testdata initializeTestData that I will include in the main method.

--- src/main/java/edu/ntnu/stud/TrainDispatchSystem.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchSystem.java
@@ -80,6 +80,66 @@ public boolean setCurrentTime(String newTimeStr) {
     return false;
 }
 
+//This method shows all traindepartures sorted after departuretime
+public void displayDepartures() {
+    System.out.println("+------------+------+-------+--------------+------------+-------+");
+    System.out.println("| Avgangstid | Linje | TogNr | Destinasjon  | Forsinkelse | Spor|");
+    System.out.println("+------------+------+-------+--------------+------------+-------+");
+    for (TrainDeparture departure : departures) {
+        System.out.println(departure.getFormattedDepartureInfo());
+    }
+    System.out.println("+------------+------+-------+--------------+------------+-------+");
+}
 
+//This method shows traindepartures planned after the current time
+public void displayDeparturesAfterCurrentTime() {
+    LocalTime current = currentTime;
+
+    List<TrainDeparture> filteredDepartures = departures.stream()
+            .filter(departure -> LocalTime.parse(departure.getDepartureTime()).isAfter(current))
+            .sorted(Comparator.comparing(TrainDeparture::getDepartureTime))
+            .collect(Collectors.toList());
+
+    if (filteredDepartures.isEmpty()) {
+        System.out.println("Ingen avganger etter nåværende tidspunkt.");
+    } else {
+        filteredDepartures.forEach(TrainDeparture::displayInformation);
+    }
 
 }
+//This method removed departures before a given time. If you change the time, departures before new time are removed
+public void removeDeparturesBefore(String time) {
+    LocalTime cutoff = LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
+
+    departures.removeIf(departure -> {
+        LocalTime departureTime = LocalTime.parse(departure.getDepartureTime(), DateTimeFormatter.ofPattern("HH:mm"));
+        LocalTime adjustedDepartureTime = departureTime;
+
+        //This part handles the delay, if there are any
+        if (!"00:00".equals(departure.getDelay()) && departure.getDelay() != null) {
+            String[] delayParts = departure.getDelay().split(":");
+            int delayHours = Integer.parseInt(delayParts[0]);
+            int delayMinutes = Integer.parseInt(delayParts[1]);
+            adjustedDepartureTime = departureTime.plusHours(delayHours).plusMinutes(delayMinutes);
+        }
+
+        //This checks if the adjusted departuretime is before cutoff
+        return adjustedDepartureTime.isBefore(cutoff);
+    });
+}
+
+//Here i added some simple testdata that I will use when launching the main method. I may move it to another class or file later on
+public void initializeTestData() {
+    addTrainDeparture(new TrainDeparture("08:30", "L1", "1001", "Oslo", 1));
+    addTrainDeparture(new TrainDeparture("09:45", "L2", "1002", "Bergen", 2));
+    addTrainDeparture(new TrainDeparture("10:15", "L3", "1003", "Trondheim", -1));
+    addTrainDeparture(new TrainDeparture("11:00", "L1", "1004", "Oslo", 3));
+    addTrainDeparture(new TrainDeparture("12:30", "L2", "1005", "Bergen", -1));
+
+    // Here I added a delay to one of the traindepartures to check its features.
+    addDelayToDeparture("1003", "00:30");
+}
+
+}
+
+
