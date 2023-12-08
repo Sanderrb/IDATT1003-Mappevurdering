15977a4da7512c02d2f5f129314a619561aee81a
Sander Rusten Berge <sanderrb@stud.ntnu.no>
2023-12-08 14:02:04 +0100

    TrainDipatchSystemTest: Added comments for the function of the test methods.

--- src/test/java/edu/ntnu/stud/TrainDispatchSystemTest.java
+++ src/test/java/edu/ntnu/stud/TrainDispatchSystemTest.java
@@ -18,6 +18,8 @@ class TrainDispatchSystemTest {
     }
 
     // Positive Tests
+
+    //Test adding and searching for a new train departure
     @Test
     void testAddAndSearchTrainDeparture() {
         TrainDeparture newDeparture = new TrainDeparture("13:00", "L2", "1006", "Bergen", 2);
@@ -25,16 +27,19 @@ class TrainDispatchSystemTest {
         assertNotNull(system.searchByTrainNumber("1006"));
     }
 
+    //Test setting track for a train departure
     @Test
     void testSetTrackForDeparture() {
         assertTrue(system.setTrackForDeparture("1001", 4));
     }
 
+    //Test adding delay to a train departure
     @Test
     void testAddDelayToDeparture() {
         assertTrue(system.addDelayToDeparture("1001", "00:30"));
     }
 
+    //Test removing train departures before a specified time
     @Test
     void testRemoveDeparturesBefore() {
         system.removeDeparturesBefore("09:00");
@@ -43,22 +48,27 @@ class TrainDispatchSystemTest {
     }
 
     // Negative Tests
+
+    //Test adding an invalid train departure
     @Test
     void testAddInvalidTrainDeparture() {
         TrainDeparture invalidDeparture = new TrainDeparture("invalid-time", "L3", "1006", "Trondheim", 3);
         assertFalse(system.addTrainDeparture(invalidDeparture));
     }
 
+    //Test setting track for a non-existent train departure
     @Test
     void testSetTrackForNonExistentDeparture() {
         assertFalse(system.setTrackForDeparture("9999", 3));
     }
 
+    //Test adding delay to a non-existent train departure
     @Test
     void testAddDelayToNonExistentDeparture() {
         assertFalse(system.addDelayToDeparture("9999", "01:00"));
     }
 
+    //Test removing train departures with an invalid time 
     @Test
     void testRemoveDeparturesWithInvalidTime() {
         assertThrows(DateTimeParseException.class, () -> {
@@ -66,6 +76,7 @@ class TrainDispatchSystemTest {
         });
     }
 
+    //Test setting the current time to the past
     @Test
 void testSetCurrentTimeToPast() {
     system.setCurrentTime(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm")));
