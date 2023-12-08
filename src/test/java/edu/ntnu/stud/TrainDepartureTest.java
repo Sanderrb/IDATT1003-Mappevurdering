e995bda04f5d7a155dcc73ecc0e5f8e7f115d8f0
Sander Rusten Berge <sanderrb@stud.ntnu.no>
2023-12-08 13:57:14 +0100

    TrainDepartureTest: Added comments for test methods.

--- src/test/java/edu/ntnu/stud/TrainDepartureTest.java
+++ src/test/java/edu/ntnu/stud/TrainDepartureTest.java
@@ -13,18 +13,22 @@ class TrainDepartureTest {
     }
 
     // Positive Tests
+
+    //Test the getters
     @Test
     void testGetters() {
         assertEquals("10:00", departure.getDepartureTime());
         assertEquals("L1", departure.getLine());
     }
 
+    //Test the setters
     @Test
     void testSetters() {
         departure.setDepartureTime("11:00");
         assertEquals("11:00", departure.getDepartureTime());
     }
-
+    
+    //Test the formatted departure info for display
     @Test
     void testFormattedDepartureInfo() {
         TrainDeparture departure = new TrainDeparture("10:00", "L1", "1001", "Oslo", 1);
@@ -36,12 +40,15 @@ class TrainDepartureTest {
 
 
     // Negative Tests
+
+    //Test setting of invalid departure time
     @Test
     void testSetInvalidDepartureTime() {
         departure.setDepartureTime("invalid-time");
         assertNotEquals("invalid-time", departure.getDepartureTime());
     }
 
+    //Test setting of invaliv track number
     @Test
     void testSetInvalidTrack() {
         departure.setTrack(-5);
